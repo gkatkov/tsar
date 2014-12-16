85c401f39645cc1564145c70ddd9d5fea186c8fe
GKatkov <gregorykatkov@gmail.com>
2014-12-16 09:32:46 +0300

    parse entries

--- src/test/groovy/com/citi/war/tsar/Test.groovy
+++ src/test/groovy/com/citi/war/tsar/Test.groovy
@@ -7,6 +7,23 @@ class Test {
 
     @org.junit.Test
     void "test" () {
-        println "aaaaa"
+        def datePrev
+        new File("src/test/resources/output-sample.log").eachLine {
+            def entry = parseEntry(it)
+            assert entry.fact.get("at7DE")
+        }
+    }
+
+
+    static Map parseEntry(String entry) {
+        def cells = entry.split(" ", 4)
+        def date = new Date(Long.valueOf(cells[0]))
+        def system = cells[1]
+        def attrsString = cells[3][5..(cells[3].size() - 2)]
+        def fact = attrsString[1..(attrsString.size() - 1)].split(",").collectEntries {
+            def a = it.trim().split(":")
+            [(a[0]): a[1]]
+        }
+        return [date: date, system: system, fact: fact]
     }
 }
